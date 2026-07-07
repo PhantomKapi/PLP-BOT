2575f74604fb90999fb85fef68b40dc09e10efcb
PhantomKapi <phantomkapi09@gmail.com>
2026-07-07 16:24:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "https://www.roblox.com/games/118737272994178/PLP-Hub",
         // Activity type number (0 = Playing).
         type: 0,
       },
